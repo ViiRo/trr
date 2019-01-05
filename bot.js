9b56bd51976d80d1d33b0a8d64bd566384ffaa55
ViiRo <45905915+ViiRo@users.noreply.github.com>
2019-01-05 14:27:52 -0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("523827935011143680")
+setInterval(function() {
+channel.send(`**Credit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit Credit**`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
